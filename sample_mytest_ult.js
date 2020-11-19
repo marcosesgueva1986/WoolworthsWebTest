2e7adbf7ac3868835bef4e570808347446cc4251
marcosesgueva1986 <68046615+marcosesgueva1986@users.noreply.github.com>
2020-11-20 01:07:52 +1100

    Update sample_mytest_ult.js

--- sample_mytest_ult.js
+++ sample_mytest_ult.js
@@ -6,8 +6,15 @@ context('Assertions', () => {
 	
 	//Recommended browser for the test: the default browser from Cypress Electron85 as it works quickly.
 	//PageTimeOut set to 80secs since Chrome takes 63secs to load the page completely.
-	cy.visit('https://www.woolworths.com.au', {timeout:80000})
 
+	cy.visit('https://www.woolworths.com.au', {timeout:80000})
+	
+	/*//.then((response) => {
+		//expect(response.status).to.eq(200)
+	//})
+  */
+  
+  
   })
 
 
@@ -26,19 +33,22 @@ context('Assertions', () => {
 
 	cy.get('[class=coreHeader-loginText]').contains('Log in / Signup').click({waitForAnimations: true})
 	
+	cy.url().should('include', '/securelogin')
 	cy.wait(6000)
-	cy.get('input[id=loginForm-Email]').type('yomarcos2@yahoo.com')
 	
-	cy.get('input[id=loginForm-Password]').type('Passw0rd')
+	cy.get('input[id=loginForm-Email]').type('yomarcos2@yahoo.com').should('have.value', 'yomarcos2@yahoo.com')
+	
+	cy.get('input[id=loginForm-Password]').type('Passw0rd').should('have.value', 'Passw0rd')
 	
 	cy.get('[id=loginForm]').submit()
 
 	
 	cy.wait(6000)
 		
-		
+	//The menu contains animations so we have to wait for the animation to load before clicking
 	cy.contains('Fruit & Veg').click({waitForAnimations: true})
 	
+	//Some browsers like Chrome were slower than Electron in the test so we have to wait a bit for the menu to load before clicking 
 	cy.wait(3000)
 
 	cy.get('[class=categoriesNavigation-linkText]').contains('Fruit').click({waitForAnimations: true})
@@ -54,8 +64,13 @@ context('Assertions', () => {
 	cy.wait(3000)
 
 	cy.contains('Add to cart').click({waitForAnimations: true})
+	cy.wait(3000)
+	
+	//after clicking the watermelon we checked it was added into the cart
+	//cy.get('[class=cart-item-name]').should('have.value', 'Watermelon')
 		
-	//check price >0, check CHeckout cart appears, 
+	//check the item was added to the the cart (cart value is not equals $0) 
+	cy.get('[class=cart-checkout-total__currency]').should('not.have.value', '$0')
 		
 
   })
