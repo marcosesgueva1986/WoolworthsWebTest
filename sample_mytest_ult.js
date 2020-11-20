4470e46e85bee9aabf1ec554ec17e8722ca01207
marcosesgueva1986 <68046615+marcosesgueva1986@users.noreply.github.com>
2020-11-21 00:29:33 +1100

    Update sample_mytest_ult.js

--- sample_mytest_ult.js
+++ sample_mytest_ult.js
@@ -2,7 +2,7 @@ context('Assertions', () => {
   beforeEach( () => {
 	
 	//The menu of the website changes depending on the resolution so we use this configuration
-	cy.viewport(1300, 620)
+	cy.viewport(1380, 680)
 	
 	//Recommended browser for the test: the default browser from Cypress Electron85 as it works quickly.
 	//PageTimeOut set to 80secs since Chrome takes 63secs to load the page completely.
@@ -35,14 +35,17 @@ context('Assertions', () => {
 	cy.get('[class=coreHeader-loginText]').contains('Log in / Signup').click({waitForAnimations: true})
 	
 	cy.url().should('include', '/securelogin')
-
 	
 	
-	cy.get('input[id=loginForm-Email]').type('yomarcos2@hotmail.com').should('have.value', 'yomarcos2@hotmail.com')
+	cy.get('input[id=loginForm-Email]').type('yomarcos2@yahoo.com').should('have.value', 'yomarcos2@yahoo.com')
 	
 	cy.get('input[id=loginForm-Password]').type('Passw0rd').should('have.value', 'Passw0rd')
 	
-	cy.get('[id=loginForm]').submit({timeout:80000}, {force: true})
+	
+	cy.get('[id=loginForm]').submit({timeout:80000})
+	cy.wait(6000)	
+	//For Chrome, do CLICK INSTEAD submit, same outcome
+	//cy.get('[class="primary-legacy m full-width mobile-full-width"]').click({waitForAnimations: true, force: true, timeout: 75000})
 
 		
 	//The menu contains animations so we have to wait for the animation to load before clicking
@@ -53,22 +56,18 @@ context('Assertions', () => {
 	//cy.wait(3000)
 
 	cy.get('[class=categoriesNavigation-linkText]').contains('Fruit').click({waitForAnimations: true})
-
 		
 	cy.contains('Melons & Mangoes').click({waitForAnimations: true})
 	
-
-		
-	cy.contains('Watermelon').click({waitForAnimations: true})
+	//cy.wait(4000)	
+	cy.contains('Watermelon').click({waitForAnimations: true, force: true}) //force: true
 	
-
-
 	cy.contains('Add to cart').click({waitForAnimations: true})
-	
-	
+		
 	//after clicking the watermelon we checked it was added into the cart
 	//cy.get('[class=cart-item-name]').should('have.value', 'Watermelon')
-		
+	
+	
 	//check the item was added to the the cart (cart value is not equals $0) 
 	cy.get('[class=cart-checkout-total__currency]').should('not.have.value', '$0')
 		
